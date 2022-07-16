3a51ff804d1001f1cde327991de6bab43f0a110d
Austin <baik.austin@gmail.com>
2022-07-16 11:06:33 -0700

    commit tree 2f7e089d28add9f0e26db714f2e4cc01c06d3408

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
+    def name=(name)
+        @name = name 
+    end
 
+    def name 
+        @name 
+    end 
+
+    attr_accessor:breed
+    def breed=(breed)
+        @breed=breed
+    end
 end
